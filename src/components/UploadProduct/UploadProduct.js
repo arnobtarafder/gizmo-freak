44c45405cccaf2f7ef795af1a22ca91b98613b49
arnobtarafder <rmtarnob@gmail.com>
2022-04-28 15:15:50 +0600

    implement: upload product

--- src/components/UploadProduct/UploadProduct.js
+++ src/components/UploadProduct/UploadProduct.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const UploadProduct = () => {
-    
+
 
     const handleUpload = event => {
         event.preventDefault();
@@ -11,6 +11,22 @@ const UploadProduct = () => {
         const price = event.target.price.value;
 
         console.log(name, price, description);
+
+        const url = `http://localhost:5000/uploadProduct`
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                name, price, description
+            }),
+            headers: {
+                "Content-type": "application/json",
+            },
+        })
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                event.target.reset()
+            })
     }
 
     return (
@@ -20,36 +36,36 @@ const UploadProduct = () => {
             <form className='w-50 mx-auto' onSubmit={handleUpload}>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Product Name</label>
-                    
-                    <input 
-                    type="text" 
-                    name='name'
-                    className="form-control" 
-                    id="exampleInputEmail1" 
-                    aria-describedby="emailHelp" />
-                    
+
+                    <input
+                        type="text"
+                        name='name'
+                        className="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp" />
+
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Description</label>
-                
-                    <input 
-                    type="text" 
-                    name='description'
-                    className="form-control" 
-                    id="exampleInputPassword1" />
+
+                    <input
+                        type="text"
+                        name='description'
+                        className="form-control"
+                        id="exampleInputPassword1" />
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Price</label>
-                
-                    <input 
-                    type="number" 
-                    name='price'
-                    className="form-control" 
-                    id="exampleInputPassword1" />
+
+                    <input
+                        type="number"
+                        name='price'
+                        className="form-control"
+                        id="exampleInputPassword1" />
                 </div>
-        
+
                 <button type="submit" className="btn btn-primary">Upload</button>
             </form>
         </div>
